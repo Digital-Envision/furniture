49a3f750f16c4f4a6b06bdddc50e9dba7e6dacc7
Muhammad Firdaus Sati <firdaus@digitalenvision.com.au>
2021-08-03 07:08:18 +0700

    Sendable files to clients

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,7 +8,15 @@ const app = express();
 
 app.use(bodyParser({ limit: '50mb' }));
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on localhost : ${PORT}`);
+});
+
+app.get('/api/:furnitures/:models', (request, response) => {
+  const { furnitures, models } = request.params;
+  response
+    .status(201)
+    .sendFile(__dirname + `/public/images/${furnitures}/${models}`);
 });
