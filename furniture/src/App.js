052eb52f30a87060689f06558a8ff914411a77b4
Muhammad Firdaus Sati <firdaus@digitalenvision.com.au>
2021-08-03 13:35:57 +0700

    Retrieveable Images from Backend

--- furniture/src/App.js
+++ furniture/src/App.js
@@ -2,15 +2,46 @@ import './App.css';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [backgrounds, setBackgrounds] = useState('');
+  const [backgrounds, setBackgrounds] = useState({});
+  const [allChair, setAllChair] = useState([]);
+  const [allCoffee, setAllCoffee] = useState([]);
+  const [allDining, setAllDining] = useState([]);
 
-  useEffect(() => {}, []);
+  const [currentChair, setCurrentChair] = useState([]);
+
+  const GetData = async () => {
+    const chairAPI = await fetch('/api/Chairs');
+    const chairPayload = await chairAPI.json();
+
+    setAllChair(chairPayload.data);
+
+    const coffeeAPI = await fetch('/api/CoffeTables');
+    const coffeePayload = await coffeeAPI.json();
+
+    setAllCoffee(coffeePayload.data);
+
+    const diningAPI = await fetch('/api/DiningTables');
+    const diningPayload = await diningAPI.json();
+
+    setAllDining(diningPayload.data);
+
+    const newChairAPI = await fetch(`/api/Chairs/${chairPayload.data[0]}`);
+    const newChairPayload = await newChairAPI.json();
+
+    setCurrentChair(newChairPayload.data);
+  };
+
+  useEffect(() => {
+    GetData();
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <div className="tables">
-          <div className="chairs left-side"></div>
+          <div className="chairs left-side">
+            <img src={currentChair[0]} alt="" />
+          </div>
           <div className="chairs center left"></div>
           <div className="chairs center right"></div>
           <div className="chairs right-side"></div>
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const path = require('path');
 
 const PORT = process.env.PORT || 3001;
@@ -14,9 +15,61 @@ app.listen(PORT, () => {
   console.log(`Server listening on localhost : ${PORT}`);
 });
 
+app.get('/api/:furnitures', (request, response) => {
+  const { furnitures } = request.params;
+
+  const dirPath = __dirname + `/public/images/${furnitures}`;
+
+  fs.readdir(dirPath, (err, models) => {
+    if (err) {
+      response.status(200).send({ data: [] });
+      return;
+    }
+
+    response.status(200).send({ data: models });
+  });
+});
+
 app.get('/api/:furnitures/:models', (request, response) => {
   const { furnitures, models } = request.params;
-  response
-    .status(201)
-    .send(__dirname + `/public/images/${furnitures}/${models}`);
+
+  const dirPath = __dirname + `/public/images/${furnitures}/${models}`;
+  const file = [];
+
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      response.status(404).send('Not Found!');
+      return;
+    }
+
+    files.forEach((names) => {
+      file.push(path.join(`images/${furnitures}/${models}`, names));
+    });
+
+    response.status(200).send({ data: file });
+  });
+});
+
+app.get('/images/:furnitures/:models/:file', (request, response) => {
+  const { furnitures, models, file } = request.params;
+  const dirPath = __dirname + `/public/images/${furnitures}/${models}/${file}`;
+
+  response.status(200).sendFile(dirPath);
+});
+
+app.get('/api/backgrounds', (request, response) => {
+  const dirPath = __dirname + `/public/images/Backgrounds`;
+
+  fs.readdir(dirPath, (err, files) => {
+    if (err) {
+      response.status(200).send({ data: [] });
+      return;
+    }
+
+    files.forEach((names) => {
+      file.push(path.join(`images/Backgrounds`, names));
+    });
+
+    response.status(200).send({ data: file });
+  });
 });
